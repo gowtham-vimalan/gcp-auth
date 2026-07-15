11b0639933507b58d77752bce60b938a64e0f65a
Gowtham <gowtham>
2026-07-15 21:21:34 +0530

    Add linked data set for analysis

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,9 @@ resource "google_logging_project_bucket_config" "log_storage" {
   retention_days = var.retention_days
   description    = var.description
 }
+
+resource "google_logging_linked_dataset" "log_analytics" {
+  link_id     = "app_logs_view"  # becomes the BigQuery dataset ID
+  bucket      = google_logging_project_bucket_config.log_storage.id
+  description = "BigQuery linked dataset for app logs"
+}
